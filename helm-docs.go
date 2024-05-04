274b33f4c6817eb4d797fa66798b4f39a510682e
roeldev <roeldev@users.noreply.github.com>
2024-05-04 16:08:03 +0200

    Update Taskfile

--- helm-docs.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Roel Schut. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// https://github.com/norwoodj/helm-docs/
-//go:generate task chart:readme
-
-package youlessobserver
