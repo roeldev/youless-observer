319f331ab6275de62d2321f05bfcf641446fe3e9
roeldev <roeldev@users.noreply.github.com>
2024-06-01 14:52:47 +0200

    Update app

---
+++ app/observer/telemetry.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2024, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package observerapp
+
+import (
+	"context"
+	"github.com/go-logr/zerologr"
+	"github.com/go-pogo/buildinfo"
+	"github.com/go-pogo/healthcheck"
+	"github.com/go-pogo/telemetry"
+	"github.com/rs/zerolog"
+	"go.opentelemetry.io/otel"
+	"sync"
+	"time"
+)
+
+func newTelemetry(conf telemetry.Config, log *zerolog.Logger, bld *buildinfo.BuildInfo, reg healthcheck.Registerer) (*telemetry.Telemetry, error) {
+	telem := telemetry.NewBuilder(conf).Global().WithDefaultExporter()
+	if bld != nil {
+		telem.TracerProvider.WithBuildInfo(bld.Internal())
+	}
+
+	if reg == nil {
+		otel.SetErrorHandler(otel.ErrorHandlerFunc(func(err error) {
+			log.Err(err).Msg("otel error")
+		}))
+	} else {
+		health := &otelHealthCheck{timeout: conf.ExporterOTLP.TimeoutDuration()}
+		reg.Register("otel.otlp_exporter", health)
+
+		otel.SetErrorHandler(otel.ErrorHandlerFunc(func(err error) {
+			log.Err(err).Msg("otel error")
+			go health.markError()
+		}))
+	}
+
+	zl := log.Level(zerolog.DebugLevel)
+	otel.SetLogger(zerologr.New(&zl))
+
+	return telem.Build()
+}
+
+var _ healthcheck.HealthChecker = (*otelHealthCheck)(nil)
+
+type otelHealthCheck struct {
+	mut     sync.RWMutex
+	lastErr time.Time
+	timeout time.Duration
+}
+
+func (o *otelHealthCheck) CheckHealth(_ context.Context) healthcheck.Status {
+	o.mut.RLock()
+	defer o.mut.RUnlock()
+
+	if time.Since(o.lastErr) <= o.timeout {
+		return healthcheck.StatusUnhealthy
+	}
+	return healthcheck.StatusHealthy
+}
+
+func (o *otelHealthCheck) markError() {
+	o.mut.Lock()
+	defer o.mut.Unlock()
+	o.lastErr = time.Now()
+}
